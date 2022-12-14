ec3e234ae401f535b64f89c0ee9e4dd1879c4dc3
victorarsene <113400146+victorarsene@users.noreply.github.com>
2022-12-14 15:50:40 +0800

    qz1

---
+++ main.c
@@ -0,0 +1,18 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main(){
+    printf("%.*s\n", 5,"*****");
+    printf("%.*s\n", 5,"*****");
+    printf("%.*s\n", 5,"*****");
+    printf("%.*s\n", 5,"*****");
+    printf("%.*s\n", 5,"*****");
+    return 0;
+}
