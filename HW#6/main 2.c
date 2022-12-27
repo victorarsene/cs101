bcc4a6ba24dc164e640ff75e65a0a821a64b89f8
victorarsene <113400146+victorarsene@users.noreply.github.com>
2022-12-28 01:00:31 +0800

    Add files via upload

---
+++ HW#6/main 2.c
@@ -0,0 +1,25 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, HTML, CSS, JS
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>  
+#include <stdlib.h>  
+  
+int main() {   
+    int i = 10; 
+
+    if( i % 2 == 0){  
+        printf("even");  
+}   
+    else{  
+        printf("odd");  
+}  
+
+    return 0;  
+}  
+
+
