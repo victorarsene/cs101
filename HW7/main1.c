73824016a8b5c520d59cbc0455dcd0824c2d46e0
victorarsene <113400146+victorarsene@users.noreply.github.com>
2022-12-29 13:12:42 +0800

    Add files via upload

---
+++ HW7/main1.c
@@ -0,0 +1,20 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main() {
+    for (int i=1;i<8;i++){
+        printf("%*s",8-i," ");
+    for (int j=0;j<i;j++){
+        printf("%d ",i);
+    }
+        printf("\n");
+    } 
+    return 0;
+}
